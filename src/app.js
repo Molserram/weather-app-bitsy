b4098c2d67e87424de0901a458863935babdd172
Molserram <moshacre@gmail.com>
2021-09-03 23:23:48 -0500

    Linked weather icons

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDate()];
-  return `${day} ${hours} ${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -29,6 +29,7 @@ function displayTemperature(response) {
   let humidityId = document.querySelector("#data-humidity");
   let windId = document.querySelector("#data-wind");
   let dateId = document.querySelector("#date");
+  let iconId = document.querySelector("#icon-main");
 
   temperatureId.innerHTML = Math.round(response.data.main.temp);
   cityId.innerHTML = response.data.name;
@@ -36,9 +37,15 @@ function displayTemperature(response) {
   humidityId.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   windId.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
   dateId.innerHTML = formatDate(response.data.dt * 1000);
+  iconId.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconId.setAttribute("alt", response.data.weather[0].description);
 }
+
 let apiKey = "8ae46e027dbc4c00e587ee2e164d9555";
-let cityName = "Montreal";
+let cityName = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
